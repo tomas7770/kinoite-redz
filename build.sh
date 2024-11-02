6a487b1bfc96f4151be2fb1db2fc6152a701b1d8
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2024-11-02 16:31:13 -0100

    Re-add RPMFusion
    Upstream UBlue replaced it with negativo17, which doesn't have NVIDIA driver 470

--- build.sh
+++ build.sh
@@ -24,11 +24,22 @@ RELEASE="$(rpm -E %fedora)"
 
 KERNEL_VERSION=$(cat /kernel-version.txt)
 
+# Install RPMFusion
+rpm-ostree install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-40.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-40.noarch.rpm
+
+# Disable Negativo 17 repo before installing RPMFusion packages
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+
 rpm-ostree install \
     krdp \
     xorg-x11-drv-nvidia-470xx \
     xorg-x11-drv-nvidia-470xx-cuda \
     /tmp/nvidia/*${KERNEL_VERSION}*.rpm \
     plasma-workspace-x11
+
+# Enable multimedia repo
+sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
 rm -rf /var/* && rm -rf /tmp/nvidia && rm -f /kernel-version.txt
